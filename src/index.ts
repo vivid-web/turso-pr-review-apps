c736fe949ffb4dedfa0979e0dff2e8e39828a19c
Peter van Meijgaard <peter@vivid-web.com>
2025-02-12 21:14:47 +0100

    Use variables instead of outputs

--- src/index.ts
+++ src/index.ts
@@ -38,8 +38,8 @@ async function run() {
 		authorization: "full-access",
 	});
 
-	core.setOutput("hostname", database.hostname);
-	core.setOutput("token", dbToken.jwt);
+	core.exportVariable("TURSO_DATABASE_URL", database.hostname);
+	core.exportVariable("TURSO_AUTH_TOKEN", dbToken.jwt);
 	core.setSecret(dbToken.jwt);
 
 	core.info(`Database ${dbName} created with hostname ${database.hostname}`);
